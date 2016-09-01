b7f5b78792901e6895f88be09957cbeb652fb4da
Samuel Vandamme <samuel@sava.be>
2016-09-01 21:48:49 +0200

    Added more python versions and fail on error

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 """Test file for Felt."""
 
 import unittest
+import sys
 
 loader = unittest.TestLoader()
 tests = loader.discover('tests', pattern='*.py')
 testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
+result = testRunner.run(tests)
+
+sys.exit(len(result.errors) + len(result.failures))
