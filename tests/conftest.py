318f2ca0d091b79c9fb385a26c1ce24dbe062c3d
Samuel Vandamme <samuel@sava.be>
2017-08-24 09:33:48 +0200

    Improved local webserver handeling

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,16 +8,18 @@ except ImportError:
     import urllib.request
 
 port = 5555
-handler = ''
+handler = None
 active = True
+thread = None
 
 def pytest_sessionstart(session):
-    global handler
+    global handler, thread
 
     print("Starting local webserver on port %s" % port)
     os.chdir('tests/source')
     handler = SocketServer.TCPServer(("", port), SimpleHTTPServer.SimpleHTTPRequestHandler)
-    thread = threading.Thread(target = loop).start()
+    thread = threading.Thread(target = loop)
+    thread.start()
 
 def loop():
     while (active):
@@ -26,7 +28,7 @@ def loop():
 def pytest_sessionfinish(session, exitstatus):
     global active
 
-    print("Closing local webserver")
+    print("\nClosing local webserver\n")
     active = False
 
     # Ping webserver to close
@@ -35,3 +37,7 @@ def pytest_sessionfinish(session, exitstatus):
         urllib2.urlopen("http://localhost:%s" % port).read()
     else:
         urllib.request.urlopen("http://localhost:%s" % port).read()
+
+    # Wait for it to finish
+    print("\nWaiting for webserver thread to end\n")
+    thread.join()
